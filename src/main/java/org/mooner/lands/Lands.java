eb3187b6f3108b82941a00a9d68cb4fbbfb4f215
Mooner510 <lmck510@gmail.com>
2022-07-30 13:13:14 +0900

    Initial commit

---
+++ src/main/java/org/mooner/lands/Lands.java
@@ -0,0 +1,17 @@
+package org.mooner.lands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
