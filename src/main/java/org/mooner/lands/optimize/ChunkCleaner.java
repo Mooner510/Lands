62e0f8a65538b4e3985d67f0d6380764deef34c0
Mooner510 <lmck510@gmail.com>
2022-09-05 18:40:33 +0900

    Ignore Some type of Entities of Chunk Cleaner

--- src/main/java/org/mooner/lands/optimize/ChunkCleaner.java
+++ src/main/java/org/mooner/lands/optimize/ChunkCleaner.java
@@ -22,9 +22,8 @@ public class ChunkCleaner implements Listener {
 
     @EventHandler
     public void onSpawn(CreatureSpawnEvent e) {
-        if(e.getEntity().getType() == EntityType.PLAYER) return;
-        if(e.getEntity() instanceof Illager) return;
-        if(e.getEntity().getType() == EntityType.VINDICATOR || e.getEntity().getType() == EntityType.VEX || e.getEntity().getType() == EntityType.STRAY) return;
+        if(e.getEntity().getType() == EntityType.PLAYER || e.getEntity().getType() == EntityType.IRON_GOLEM) return;
+        if(e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.RAID || e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.PATROL) return;
         final double random = Math.random();
         e.setCancelled(switch (e.getEntityType()) {
             case ZOMBIFIED_PIGLIN -> random <= 0.8;
@@ -32,7 +31,7 @@ public class ChunkCleaner implements Listener {
             default -> false;
         });
         e.setCancelled(e.isCancelled() || switch (e.getSpawnReason()) {
-            case NATURAL, SPAWNER, EGG, JOCKEY, BREEDING, MOUNT ->
+            case NATURAL, SPAWNER, EGG, JOCKEY, BREEDING, MOUNT, BUILD_SNOWMAN ->
                     e.getLocation().getChunk().getEntities().length > MAX_ENTITY_IN_CHUNK;
 //            case VILLAGE_DEFENSE -> {
 //
